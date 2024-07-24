d46711a590a92779e7fbd53d4629aa0e7ba92148
Scott-C-Walton <scottcwalton@outlook.com>
2024-07-24 18:50:45 -0400

    more updates

--- Python/Day2/ex1.py
+++ Python/Day2/ex1.py
@@ -1,1 +1,11 @@
 # Still a placeholder
+
+height = input("How tall are you in meters? ")
+weight = input("How much do you weigh in kg? ")
+
+weight_as_int = int(weight)
+height_as_float = float(height)
+
+bmi = int(weight_as_int / height_as_float ** 2)
+
+print(f"Your BMI is {bmi}.")
---
+++ Python/Day2/ex2.py
@@ -0,0 +1,10 @@
+score = 0
+
+# Takes the previous value of the variable (score in this case) and performs maths on it to assign
+# a new value to that variable
+score += 3
+
+print(score)
+
+# Example of f string
+print(f"Your score is {score}")
